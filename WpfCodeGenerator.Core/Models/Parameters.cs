555536ace32ac65f84ed3e7d3bcc3b310b179043
Xavier Mathieu <xavier2003@hotmail.fr>
2022-04-15 16:03:06 -0400

    Feat: ViewModel puts variables in Parameters

---
+++ WpfCodeGenerator.Core/Models/Parameters.cs
@@ -0,0 +1,20 @@
+﻿namespace WpfCodeGenerator.Core.Models;
+
+public class Parameters
+{
+    private string _json;
+    private string _className;
+    private string _language;
+
+    public Parameters(string json, string className, string language)
+    {
+        _json = json;
+        _className = className;
+        _language = language;
+    }
+
+    public void Validate()
+    {
+        
+    }
+}
--- WpfCodeGenerator.Core/ViewModels/GeneratorViewModel.cs
+++ WpfCodeGenerator.Core/ViewModels/GeneratorViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace WpfCodeGenerator.Core.ViewModels;
+﻿using WpfCodeGenerator.Core.Models;
+
+namespace WpfCodeGenerator.Core.ViewModels;
 
 public class GeneratorViewModel
 {
     public GeneratorViewModel(string json, string className, string language)
     {
-        
+        var parameters = new Parameters(json, className, language);
+        parameters.Validate();
     }
 }
