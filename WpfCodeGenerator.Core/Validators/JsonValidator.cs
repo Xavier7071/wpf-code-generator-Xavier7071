158c9dfcf97d0f2d6a06093caef3e485aed0b549
Xavier Mathieu <xavier2003@hotmail.fr>
2022-04-15 16:23:16 -0400

    Feat: Added a validator for json

---
+++ WpfCodeGenerator.Core/Validators/JsonValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace WpfCodeGenerator.Core.Validators;
+
+public class JsonValidator
+{
+    public string Error { get; private set; }
+    
+    public JsonValidator(string json)
+    {
+        Error = "";
+        Validate(json);
+    }
+
+    private void Validate(string json)
+    {
+        if (json.Length == 0)
+        {
+            Error = "JSON (Le champ doit être rempli)";
+        }
+        else
+        {
+            try
+            {
+                JsonDocument.Parse(json);
+            }
+            catch (Exception)
+            {
+                Error = "JSON (Le JSON doit être valide)";
+            }
+        }
+    }
+}
