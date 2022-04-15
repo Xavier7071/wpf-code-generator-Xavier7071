1a199cb48f2d4240e73e26bd583c2d06ff824cf0
Xavier Mathieu <xavier2003@hotmail.fr>
2022-04-15 16:20:50 -0400

    Feat: Added validator for the class name

---
+++ WpfCodeGenerator.Core/Validators/ClassNameValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace WpfCodeGenerator.Core.Validators;
+
+public class ClassNameValidator
+{
+    public string Error { get; private set; }
+    
+    public ClassNameValidator(string className)
+    {
+        Error = "";
+        Validate(className);
+    }
+
+    private void Validate(string className)
+    {
+        if (className.Length == 0)
+        {
+            Error = "Nom de la classe (Le champ doit être rempli)";
+        }
+        else if (!className.Contains('.'))
+        {
+            Error = "Nom de la classe (Le champ doit être valide)";
+        }
+    }
+}
