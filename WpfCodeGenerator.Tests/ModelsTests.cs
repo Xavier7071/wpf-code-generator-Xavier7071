216ef06039a20202d76718895edbde3c242aa819
Xavier Mathieu <xavier2003@hotmail.fr>
2022-04-16 19:08:38 -0400

    Feat: Added XUnit and tests classes

---
+++ WpfCodeGenerator.Tests/ModelsTests.cs
@@ -0,0 +1,5 @@
+﻿namespace WpfCodeGenerator.Tests;
+
+public class ModelsTests
+{
+}
---
+++ WpfCodeGenerator.Tests/ServicesTests.cs
@@ -0,0 +1,29 @@
+﻿using WpfCodeGenerator.Core.Services;
+using Xunit;
+
+namespace WpfCodeGenerator.Tests;
+
+public class ServicesTests
+{
+    [Fact]
+    public void ValidNames()
+    {
+        var list = new List<string>
+        {
+            "salut.test",
+            "bonjour"
+        };
+        Assert.Equal(list, GeneratorService.GetNames("salut.test.bonjour"));
+    }
+
+    [Fact]
+    public void InvalidNames()
+    {
+        var list = new List<string>
+        {
+            "salut.test.",
+            "bonjour"
+        };
+        Assert.NotEqual(list, GeneratorService.GetNames("salut.test.bonjour"));
+    }
+}
