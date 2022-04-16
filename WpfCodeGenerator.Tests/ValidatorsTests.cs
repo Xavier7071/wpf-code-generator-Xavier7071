c9821c4f9ad8a46ea78ca01b8c438badb655b2f6
Xavier Mathieu <xavier2003@hotmail.fr>
2022-04-16 19:15:09 -0400

    Feat: Added ValidatorsTests

--- WpfCodeGenerator.Tests/ValidatorsTests.cs
+++ WpfCodeGenerator.Tests/ValidatorsTests.cs
@@ -1,5 +1,36 @@
-﻿namespace WpfCodeGenerator.Tests;
+﻿using WpfCodeGenerator.Core.Validators;
+using Xunit;
 
-public class ValidatorsTests
+namespace WpfCodeGenerator.Tests;
+
+public class ValidatorTests
 {
+    [Fact]
+    public void ValidClassName()
+    {
+        var classNameValidator = new ClassNameValidator("test.bonjour");
+        Assert.Equal("", classNameValidator.Error);
+    }
+
+    [Fact]
+    public void InvalidClassName()
+    {
+        var classNameValidator = new ClassNameValidator("test");
+        Assert.NotEqual("", classNameValidator.Error);
+    }
+
+    [Fact]
+    public void ValidJson()
+    {
+        var jsonValidator = new JsonValidator(
+            "{\n  \"languages\": [\n    {\n      \"language\": \"English\",\n      \"image\": \"imagePath\",\n      \"jsonPath\": \"jsonPath\"\n    },\n    {\n      \"language\": \"Français\",\n      \"image\": \"imagePath\",\n      \"jsonPath\": \"jsonPath\"\n    },\n    {\n      \"language\": \"Español\",\n      \"image\": \"imagePath\",\n      \"jsonPath\": \"jsonPath\"\n    }\n  ]\n}");
+        Assert.Equal("", jsonValidator.Error);
+    }
+
+    [Fact]
+    public void InvalidJson()
+    {
+        var jsonValidator = new JsonValidator("invalid json");
+        Assert.NotEqual("", jsonValidator.Error);
+    }
 }
